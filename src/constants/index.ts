b2deecf84dbaa3d0f0e1f2e8dd433b9b1f5cea8c
Luke Morales <lukemorales@live.com>
2021-10-04 20:29:26 -0300

    chore: setup axios client

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,6 @@ export const IS_DEV = process.env.NODE_ENV === 'development';
 
 export const IS_MSW_ENABLED =
   IS_DEV && process.env.REACT_APP_MSW_ENABLED === 'true';
+
+export const API_BASE_URL =
+  process.env.REACT_APP_BASE_URL || 'http://localhost:3333';
